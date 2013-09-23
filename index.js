ac81cd957bc950771a586a5f1aca9a318b62b3a4
Zack Owens <zowens2009@gmail.com>
2013-09-22 21:56:58 -0400

    adding redis subscriber/publisher

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ var port = process.env.port || 8080;
 var wss = new (require('ws').Server)({ port: port });
 var sockets = [];
 
-wss.on('connection', function(ws) {
-    sockets.push(ws);
+var redis = require('redis'),
+    redisSubscriber, redisPublisher;
 
-    ws.on('message', function(msg) {
-        console.log(msg);
-        // broadcast the message over to the other sockets
+
+
+function listenToWebSockets() {
+    wss.on('connection', function(ws) {
+        sockets.push(ws);
+
+        ws.on('message', function(msg) {
+            // broadcast the message over Redis to the other
+            // sockets on this and other servers
+            redisPublisher.publish('webrtc', msg);
+        });
+
+        ws.on('close', function() {
+            // remove the socket from the list of sockets
+            var id = sockets.indexOf(ws);
+            if (id >= 0) {
+                sockets.splice(id, 1);
+            }
+
+            console.log('socket closed');
+        });
+
+        console.log('socket connected');
+    });
+}
+
+function listenToRedis() {
+    // subscribe on the subscriber client to all messages,
+    // and then post these messages over to the web sockets
+    redisSubscriber.on('message', function(channel, msg) {
+        console.log('redis subscriber: ' + msg);
         sockets.forEach(function(s) {
             try {
                 s.send(msg); 
@@ -17,20 +45,37 @@ wss.on('connection', function(ws) {
             }
         });
     });
+    redisSubscriber.subscribe('webrtc');
+}
 
-    ws.on('close', function() {
-        // remove the socket from the list of sockets
-        var id = sockets.indexOf(ws);
-        if (id >= 0) {
-            sockets.splice(id, 1);
+if (process.env.NODE_ENV === 'production') {
+    redisSubscriber = redis.createClient(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST, {no_ready_check: true});
+    redisPublisher = redis.createClient(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST, {no_ready_check: true});
+
+    redisSubscriber.auth(process.env.REDIS_KEY, function(err) {
+        if (err) {
+            console.error("Unable to connect to redis");
+            return;
         }
+        console.log('a');
 
-        console.log('socket closed');
+        redisPublisher.auth(process.env.REDIS_KEY, function(err) {
+            if (err) {
+                console.error("Unable to connect to redis");
+                return;
+            }
+        console.log('a');
+            listenToRedis();
+            listenToWebSockets();
+        });
     });
 
-    console.log('socket connected');
-});
+} else {
+    redisSubscriber = redis.createClient();
+    redisPublisher = redis.createClient();
+    listenToRedis();
+    listenToWebSockets();
 
-if (process.env.NODE_ENV !== 'production') {
+    // start the file server on localhost:8081
     require('./fileClient');
 }
